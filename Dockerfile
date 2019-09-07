e1cf1659f4cad5790787f81cf3469668005c7ea0
JoeLamptey <rutherford.gh@gmail.com>
2019-09-07 02:40:02 +0000

    improved layout for the web app

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
+# FROM alpine
+# ENV FLASK_APP app.py
+# ENV FLASK_RUN_HOST 0.0.0.0
+# COPY requirements.txt requirements.txt
+# RUN apk add --no-cache --update python3 && pip3 install --upgrade pip setuptools
+# RUN pip3 install -r requirements.txt
+# COPY . .
+# CMD ["flask", "run"]
 
-FROM alpine
-COPY manipulator.py /manipulator.py
-RUN  apk add python3
-
+# FROM alpine
+# COPY manipulator.py /manipulator.py
+# RUN  apk add python3
+# CMD ["python3", "manipulator.py"]
  
 # FROM alpine
 # COPY controller.py controller.py
-# RUN apk add python3 
-# CMD ["python3", "controller.py"]
+# RUN apk add python3
+# CMD ["python3", "controller.py"] 
 
-# WORKDIR /code
-# ENV FLASK_APP app.py
-# ENV FLASK_RUN_HOST 0.0.0.0
-# RUN apk add py3-numpy
-# RUN apk add python3 
-# COPY requirements.txt requirements.txt
-# RUN pip install -r requirements.txt
-# COPY . .
-# CMD ["flask", "run"]
 # sudo docker inspect 1e75ae5c225f --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return app.make_response('Hello Universe!')
+    # return app.make_response('Hello Universe!')
+    user = {"username": "System"}
+    mani = [
+        {'datetime': '2019-09-06 22:37', 'status': ['up', 'dowm']},
+        {'datetime': '2019-09-06 22:37', 'status': ['up', 'dowm']},
+        {'datetime': '2019-09-06 22:37', 'status': ['up', 'dowm']},
+        {'datetime': '2019-09-06 22:39', 'status': ['up', 'dowm']},
+        {'datetime': '2019-09-06 22:39', 'status': ['up', 'dowm']},
+        {'datetime': '2019-09-06 22:39', 'status': ['up', 'dowm']}        
+    ]
+
+    controller = [
+        {'datetime': '2019-09-06 22:37', 'status': ['up', 'dowm'], 'decision':'10:05'},
+        {'datetime': '2019-09-06 22:37', 'status': ['up', 'dowm'], 'decision':'11:25'},
+        {'datetime': '2019-09-06 22:39', 'status': ['up', 'dowm'], 'decision':'3:25'},
+        {'datetime': '2019-09-06 22:39', 'status': ['up', 'dowm'], 'decision':'7:25'},
+        {'datetime': '2019-09-06 22:39', 'status': ['up', 'dowm'], 'decision':'12:25'}        
+    ]
+
+    return render_template("index.html", title="Python Backend - Test", user=user, mani=mani, controller=controller)
+
+if __name__ == "__main__":
+    app.run(debug=True)
